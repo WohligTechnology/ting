a230e1f38034831a189d1dd6a9ffe0a7652d0137
Pooja <pthakare33@gmail.com>
2015-10-21 12:10:51 +0530

    db added

---
+++ ting (1).sql
@@ -0,0 +1,486 @@
+-- phpMyAdmin SQL Dump
+-- version 4.3.11
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 21, 2015 at 08:38 AM
+-- Server version: 5.6.24
+-- PHP Version: 5.5.24
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `ting`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `accesslevel`
+--
+
+CREATE TABLE IF NOT EXISTS `accesslevel` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `accesslevel`
+--
+
+INSERT INTO `accesslevel` (`id`, `name`) VALUES
+(1, 'admin'),
+(2, 'Operator'),
+(3, 'User');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `logintype`
+--
+
+CREATE TABLE IF NOT EXISTS `logintype` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `logintype`
+--
+
+INSERT INTO `logintype` (`id`, `name`) VALUES
+(1, 'Facebook'),
+(2, 'Twitter'),
+(3, 'Email'),
+(4, 'Google');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `menu`
+--
+
+CREATE TABLE IF NOT EXISTS `menu` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `keyword` varchar(255) NOT NULL,
+  `url` text NOT NULL,
+  `linktype` int(11) NOT NULL,
+  `parent` int(11) NOT NULL,
+  `isactive` int(11) NOT NULL,
+  `order` int(11) NOT NULL,
+  `icon` varchar(255) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `menu`
+--
+
+INSERT INTO `menu` (`id`, `name`, `description`, `keyword`, `url`, `linktype`, `parent`, `isactive`, `order`, `icon`) VALUES
+(2, 'Videos', '', '', 'site/viewvideo', 1, 0, 1, 2, 'icon-dashboard'),
+(3, 'Digital Marketing', '', '', 'site/viewdigitalmarketing', 1, 0, 1, 1, 'icon-dashboard'),
+(5, 'Apps', '', '', 'site/viewapps', 1, 0, 1, 0, 'icon-dashboard'),
+(6, 'Website', '', '', 'site/viewwebsite', 1, 0, 1, 3, 'icon-dashboard'),
+(25, 'Users', '', '', 'site/viewusers', 1, 0, 0, 1, 'icon-user');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `menuaccess`
+--
+
+CREATE TABLE IF NOT EXISTS `menuaccess` (
+  `menu` int(11) NOT NULL,
+  `access` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `menuaccess`
+--
+
+INSERT INTO `menuaccess` (`menu`, `access`) VALUES
+(1, 1),
+(4, 1),
+(2, 1),
+(3, 1),
+(5, 1),
+(6, 1),
+(7, 1),
+(7, 3),
+(8, 1),
+(9, 1),
+(10, 1),
+(11, 1),
+(12, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `statuses`
+--
+
+CREATE TABLE IF NOT EXISTS `statuses` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `statuses`
+--
+
+INSERT INTO `statuses` (`id`, `name`) VALUES
+(1, 'inactive'),
+(2, 'Active'),
+(3, 'Waiting'),
+(4, 'Active Waiting'),
+(5, 'Blocked');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ting_apps`
+--
+
+CREATE TABLE IF NOT EXISTS `ting_apps` (
+  `id` int(11) NOT NULL,
+  `order` int(11) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `title` varchar(255) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `ting_apps`
+--
+
+INSERT INTO `ting_apps` (`id`, `order`, `image`, `title`) VALUES
+(1, 1, 'download_(2)7.jpg', 'whatsapp'),
+(2, 2, 'download_(2).jpg', 'doll');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ting_digitalmarketing`
+--
+
+CREATE TABLE IF NOT EXISTS `ting_digitalmarketing` (
+  `id` int(11) NOT NULL,
+  `order` int(11) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `facebooklink` varchar(255) NOT NULL,
+  `twitterlink` varchar(255) NOT NULL,
+  `googlelink` varchar(255) NOT NULL,
+  `linkedinlink` varchar(255) NOT NULL,
+  `instagramlink` varchar(255) NOT NULL,
+  `youtubelink` varchar(255) NOT NULL,
+  `pinterestlink` varchar(255) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `ting_digitalmarketing`
+--
+
+INSERT INTO `ting_digitalmarketing` (`id`, `order`, `image`, `facebooklink`, `twitterlink`, `googlelink`, `linkedinlink`, `instagramlink`, `youtubelink`, `pinterestlink`) VALUES
+(1, 2, 'ArmaanPlainParatha20PFinalOP.jpg', 'fb.com', 'twi.com', 'google.com', 'link.com', 'insta.com', 'you.com', 'pinterest.com'),
+(2, 1, 'delorean.jpg', 'qWzret', 'ghj', 'vgbh', 'cvg', 'bhc', 'vgbhcvgbh', 'fgh'),
+(3, 0, 'Event-management3.png', '', '', '', '', '', '', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ting_video`
+--
+
+CREATE TABLE IF NOT EXISTS `ting_video` (
+  `id` int(11) NOT NULL,
+  `order` int(11) NOT NULL,
+  `videourl` varchar(255) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `image` varchar(255) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `ting_video`
+--
+
+INSERT INTO `ting_video` (`id`, `order`, `videourl`, `title`, `image`) VALUES
+(1, 2, 'sfweWE', 'Office', 'download_(2)4.jpg'),
+(3, 1, 'cgvhb', 'Recipes', 'delorean1.jpg'),
+(4, 0, '', '', 'download_(3).jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ting_website`
+--
+
+CREATE TABLE IF NOT EXISTS `ting_website` (
+  `id` int(11) NOT NULL,
+  `order` int(11) NOT NULL,
+  `type` int(11) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `title` varchar(255) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `ting_website`
+--
+
+INSERT INTO `ting_website` (`id`, `order`, `type`, `image`, `title`) VALUES
+(1, 3, 1, 'available_at1.JPG', 'heyy'),
+(2, 1, 2, 'images_(1).jpg', 'www.'),
+(21, 2, 0, 'delorean3.jpg', 'qetryt');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `type`
+--
+
+CREATE TABLE IF NOT EXISTS `type` (
+  `id` int(11) NOT NULL,
+  `website` int(11) NOT NULL,
+  `type` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `type`
+--
+
+INSERT INTO `type` (`id`, `website`, `type`) VALUES
+(1, 1, 1),
+(2, 1, 2),
+(3, 1, 3),
+(4, 2, 3),
+(5, 2, 4),
+(6, 2, 5),
+(7, 21, 2),
+(8, 21, 3),
+(9, 21, 4);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `accesslevel` int(11) DEFAULT NULL,
+  `timestamp` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `status` int(11) DEFAULT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  `username` varchar(255) NOT NULL,
+  `socialid` varchar(255) NOT NULL,
+  `logintype` int(11) NOT NULL,
+  `json` text NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `name`, `password`, `email`, `accesslevel`, `timestamp`, `status`, `image`, `username`, `socialid`, `logintype`, `json`) VALUES
+(1, 'wohlig', 'a63526467438df9566c508027d9cb06b', 'wohlig@wohlig.com', 1, '0000-00-00 00:00:00', 1, NULL, '', '', 0, ''),
+(4, 'pratik', '0cb2b62754dfd12b6ed0161d4b447df7', 'pratik@wohlig.com', 1, '2014-05-12 06:52:44', 1, NULL, 'pratik', '1', 1, ''),
+(5, 'wohlig123', 'wohlig123', 'wohlig1@wohlig.com', 1, '2014-05-12 06:52:44', 1, NULL, '', '', 0, ''),
+(6, 'wohlig1', 'a63526467438df9566c508027d9cb06b', 'wohlig2@wohlig.com', 1, '2014-05-12 06:52:44', 1, NULL, '', '', 0, ''),
+(7, 'Avinash', '7b0a80efe0d324e937bbfc7716fb15d3', 'avinash@wohlig.com', 1, '2014-10-17 06:22:29', 1, NULL, '', '', 0, ''),
+(9, 'avinash', 'a208e5837519309129fa466b0c68396b', 'a@email.com', 2, '2014-12-03 11:06:19', 3, '', '', '123', 1, 'demojson'),
+(13, 'aaa', 'a208e5837519309129fa466b0c68396b', 'aaa3@email.com', 3, '2014-12-04 06:55:42', 3, NULL, '', '1', 2, 'userjson'),
+(15, 'jagss', '4bcc674371a91bf32377cd878d754527', 'jass@gmail.com', 1, '2015-10-19 08:09:08', 2, 'available_at2.JPG', '', 'cfgvhbn', 0, '0');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userlog`
+--
+
+CREATE TABLE IF NOT EXISTS `userlog` (
+  `id` int(11) NOT NULL,
+  `onuser` int(11) NOT NULL,
+  `status` int(11) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `userlog`
+--
+
+INSERT INTO `userlog` (`id`, `onuser`, `status`, `description`, `timestamp`) VALUES
+(1, 1, 1, 'User Address Edited', '2014-05-12 06:50:21'),
+(2, 1, 1, 'User Details Edited', '2014-05-12 06:51:43'),
+(3, 1, 1, 'User Details Edited', '2014-05-12 06:51:53'),
+(4, 4, 1, 'User Created', '2014-05-12 06:52:44'),
+(5, 4, 1, 'User Address Edited', '2014-05-12 12:31:48'),
+(6, 23, 2, 'User Created', '2014-10-07 06:46:55'),
+(7, 24, 2, 'User Created', '2014-10-07 06:48:25'),
+(8, 25, 2, 'User Created', '2014-10-07 06:49:04'),
+(9, 26, 2, 'User Created', '2014-10-07 06:49:16'),
+(10, 27, 2, 'User Created', '2014-10-07 06:52:18'),
+(11, 28, 2, 'User Created', '2014-10-07 06:52:45'),
+(12, 29, 2, 'User Created', '2014-10-07 06:53:10'),
+(13, 30, 2, 'User Created', '2014-10-07 06:53:33'),
+(14, 31, 2, 'User Created', '2014-10-07 06:55:03'),
+(15, 32, 2, 'User Created', '2014-10-07 06:55:33'),
+(16, 33, 2, 'User Created', '2014-10-07 06:59:32'),
+(17, 34, 2, 'User Created', '2014-10-07 07:01:18'),
+(18, 35, 2, 'User Created', '2014-10-07 07:01:50'),
+(19, 34, 2, 'User Details Edited', '2014-10-07 07:04:34'),
+(20, 18, 2, 'User Details Edited', '2014-10-07 07:05:11'),
+(21, 18, 2, 'User Details Edited', '2014-10-07 07:05:45'),
+(22, 18, 2, 'User Details Edited', '2014-10-07 07:06:03'),
+(23, 7, 6, 'User Created', '2014-10-17 06:22:29'),
+(24, 7, 6, 'User Details Edited', '2014-10-17 06:32:22'),
+(25, 7, 6, 'User Details Edited', '2014-10-17 06:32:37'),
+(26, 8, 6, 'User Created', '2014-11-15 12:05:52'),
+(27, 9, 6, 'User Created', '2014-12-02 10:46:36'),
+(28, 9, 6, 'User Details Edited', '2014-12-02 10:47:34'),
+(29, 4, 6, 'User Details Edited', '2014-12-03 10:34:49'),
+(30, 4, 6, 'User Details Edited', '2014-12-03 10:36:34'),
+(31, 4, 6, 'User Details Edited', '2014-12-03 10:36:49'),
+(32, 8, 6, 'User Details Edited', '2014-12-03 10:47:16');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `accesslevel`
+--
+ALTER TABLE `accesslevel`
+  ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `name` (`name`);
+
+--
+-- Indexes for table `logintype`
+--
+ALTER TABLE `logintype`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `menu`
+--
+ALTER TABLE `menu`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `statuses`
+--
+ALTER TABLE `statuses`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `ting_apps`
+--
+ALTER TABLE `ting_apps`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `ting_digitalmarketing`
+--
+ALTER TABLE `ting_digitalmarketing`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `ting_video`
+--
+ALTER TABLE `ting_video`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `ting_website`
+--
+ALTER TABLE `ting_website`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `type`
+--
+ALTER TABLE `type`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `userlog`
+--
+ALTER TABLE `userlog`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `accesslevel`
+--
+ALTER TABLE `accesslevel`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `logintype`
+--
+ALTER TABLE `logintype`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `menu`
+--
+ALTER TABLE `menu`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT for table `statuses`
+--
+ALTER TABLE `statuses`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT for table `ting_apps`
+--
+ALTER TABLE `ting_apps`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `ting_digitalmarketing`
+--
+ALTER TABLE `ting_digitalmarketing`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `ting_video`
+--
+ALTER TABLE `ting_video`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `ting_website`
+--
+ALTER TABLE `ting_website`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=22;
+--
+-- AUTO_INCREMENT for table `type`
+--
+ALTER TABLE `type`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=17;
+--
+-- AUTO_INCREMENT for table `userlog`
+--
+ALTER TABLE `userlog`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=33;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
